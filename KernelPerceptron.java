1d7f0fca69f88cf2bb13df71cd8973ee2656afed
Harriet Willmott <harriet.willmott@gmail.com>
2013-02-18 14:23:50 -0800

    fixed perceptron so it actually works. implementation is still crude and there are a lot of print statements. going to put in elegant I/O and clean it up a bit

--- KernelPerceptron.java
+++ KernelPerceptron.java
@@ -1,4 +1,4 @@
-
+import java.lang.Math.*;
 
 public class KernelPerceptron {
 	
@@ -6,18 +6,60 @@ public class KernelPerceptron {
 	{
 		int NUM_SAMPLES = 10;
 		int d = 3;
+		
 		int[] x = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		int[] y = {-1, -1, -1, -1, 1, 1, 1, 1, 1, 1};
+		int[] c = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		int[][] K = new int[NUM_SAMPLES][NUM_SAMPLES];
 		
+		int i, j, k;
+		int sum;
+		int count = 0;
+		
+		boolean misclassified = true;
+		
+		System.out.println("Kernel Matrix");
 		// calculate kernel matrix
-		for(int i = 0; i < NUM_SAMPLES; i++)
+		for(i = 0; i < NUM_SAMPLES; i++)
 		{
-			for(int j = 0; j < NUM_SAMPLES; j++)
+			for(j = 0; j < NUM_SAMPLES; j++)
 			{
-				K[i][j] = (1 + x[i]*y[i])^d;
+				K[i][j] = (int) Math.pow(1 + x[i]*y[j], d);
+				System.out.print(K[i][j] + " ");
 			}
+			System.out.println();
 		}
+		
+		while(misclassified && (count < 1000))
+		{
+			count++;
+			misclassified = false;
+			for(j=0; j < NUM_SAMPLES; j++)
+			{
+				sum = 0;
+				for(k = 0; k < NUM_SAMPLES; k++)
+				{
+					sum += c[k]*K[k][j];	
+				}
+				sum *= y[j];
+				
+				if(sum <= 0)
+				{
+					c[j] += y[j];
+					misclassified = true;
+				}
+			}
+			
+		}
+		
+		System.out.println();
+		if(misclassified) System.out.println("did not converge after 1000 iterations");
+		System.out.print("c = ");
+		for(i = 0; i < NUM_SAMPLES; i++)
+		{
+			System.out.print(c[i] + " ");
+		}
+		System.out.println();
 	}
 
 	
