6cce5311a9b8455d7e322f70c6c30abc095bb34d
Harriet Willmott <harriet.willmott@gmail.com>
2013-02-18 18:43:00 -0800

    kernelperceptron way prettier and some input validation but not really

--- KernelPerceptron.java
+++ KernelPerceptron.java
@@ -1,66 +1,131 @@
+import java.io.*;
 import java.lang.Math.*;
+import java.util.*;
 
 public class KernelPerceptron {
-	
+	static int[] x;
+	static int[] y;
+	static int[][] K;
+	static int num_samples;
+	static int max_iterations;
+
+	static boolean debug = false;
+
 	public static void main(String[] args)
 	{
-		int NUM_SAMPLES = 10;
-		int d = 3;
+		/* usage: java KernelPerceptron <inputfile> <max iterations> <kernel dimension>
+		 * <input file> txt file with number of samples as first line and every line following contains two ints, <x,y>
+		 * <max iterations> is the number of iterations the program should perform before giving up on classifying the samples
+		 * <kernel dimension> is the dimension mapped to
+		 */
+		if( args.length != 3)
+		{
+			System.out.println("usage: java KernelPerceptron <inputfile> <max iterations> <kernel dimension>\n" +
+					" <input file> txt file with number of samples as first line and every line following contains two ints, <x,y> \n" +
+					" <max iterations> is the number of iterations the program should perform before giving up on classifying the samples\n " +
+					" <kernel dimension> is the dimension mapped to");
+		}
+
+		// read in the samples
+		File infile = new File(args[0]);
+		Scanner in;
+		try {
+			in = new Scanner(infile);
+			parseInput(in);
+		} catch (FileNotFoundException e) {
+			System.out.println("error reading file");
+			e.printStackTrace();
+		}
 		
-		int[] x = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		int[] y = {-1, -1, -1, -1, 1, 1, 1, 1, 1, 1};
-		int[] c = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		int[][] K = new int[NUM_SAMPLES][NUM_SAMPLES];
-		
+
+		// other logistical stuff
+		max_iterations = Integer.parseInt(args[1]);
+		int dimension = Integer.parseInt(args[2]);
+
+		// compute the kernel matrix
+		computeKernel(dimension);
+
+		int[] c = classify();
+
+
+	}
+
+	public static void parseInput(Scanner in)
+	{
+		num_samples = in.nextInt();
+		x = new int[num_samples];
+		y = new int[num_samples];
+
+		if(debug) System.out.println("num samples: " + num_samples);
+		for(int i = 0; i < num_samples; i++)
+		{
+			x[i] = in.nextInt();
+			y[i] = in.nextInt();		
+		}
+	}
+
+	public static void computeKernel(int dimension)
+	{
+		if(debug) System.out.println("Kernel Matrix");
+		K = new int[num_samples][num_samples];
+		for(int i = 0; i < num_samples; i++)
+		{
+			for(int j = 0; j < num_samples; j++)
+			{
+				K[i][j] = (int) Math.pow(1 + x[i]*y[j], dimension);
+				if(debug) System.out.print(K[i][j] + " ");
+			}
+			if(debug) System.out.println();
+		}
+
+	}
+
+	public static int[] classify()
+	{
 		int i, j, k;
 		int sum;
 		int count = 0;
-		
+
+		// the classifier
+		int[] c = new int[num_samples];
+		Arrays.fill(c, 0);
+
+		// loop control
 		boolean misclassified = true;
-		
-		System.out.println("Kernel Matrix");
-		// calculate kernel matrix
-		for(i = 0; i < NUM_SAMPLES; i++)
-		{
-			for(j = 0; j < NUM_SAMPLES; j++)
-			{
-				K[i][j] = (int) Math.pow(1 + x[i]*y[j], d);
-				System.out.print(K[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-		while(misclassified && (count < 1000))
+
+		while(misclassified && (count < max_iterations))
 		{
 			count++;
 			misclassified = false;
-			for(j=0; j < NUM_SAMPLES; j++)
+			for(j=0; j < num_samples; j++)
 			{
 				sum = 0;
-				for(k = 0; k < NUM_SAMPLES; k++)
+				for(k = 0; k < num_samples; k++)
 				{
 					sum += c[k]*K[k][j];	
 				}
 				sum *= y[j];
-				
+
 				if(sum <= 0)
 				{
 					c[j] += y[j];
 					misclassified = true;
 				}
 			}
-			
+
 		}
-		
+
 		System.out.println();
-		if(misclassified) System.out.println("did not converge after 1000 iterations");
+		if(misclassified) System.out.println("did not converge after " + max_iterations + "  iterations");
 		System.out.print("c = ");
-		for(i = 0; i < NUM_SAMPLES; i++)
+		for(i = 0; i < num_samples; i++)
 		{
 			System.out.print(c[i] + " ");
 		}
 		System.out.println();
+		
+		
+		return c;
 	}
-
-	
 }
+
