fa822f7f55a1419c77dfda226ba819e4abc07ee4
GriffinLedingham <gcledingham@gmail.com>
2013-03-03 14:07:52 -0800

    r6 Formatted code.

--- DecisionStumps.java
+++ DecisionStumps.java
@@ -28,27 +28,33 @@ public class DecisionStumps {
 		build(numIterations);
 	}
 	
-	private void build(int numIterations) {
+	private void build(int numIterations)
+    {
 		int m = trainingSetSize;
         int n = trainingSetXY[0].length;
         float minErr = Float.POSITIVE_INFINITY;
         float numStep = 10.0f;
         float[] bestClassEst = new float[m];
-        for(int i=0; i<m; i++) {
+        for(int i=0; i<m; i++)
+        {
             bestClassEst[i]=0.0f;
         }
-        for(int i=0; i<n; i++) { //iterate over each attribute x, y, etc..
-            float min = trainingSetXY[0][i]; 
-            float max = trainingSetXY[0][i]; 
-            for(int j=0;j<trainingSetXY.length;j++) {
+        for(int i=0; i<n; i++) //iterate over each attribute x, y, etc..
+        {
+            float min = trainingSetXY[0][i];
+            float max = trainingSetXY[0][i];
+            for(int j=0;j<trainingSetXY.length;j++)
+            {
                 if(trainingSetXY[j][i] < min) //find min value
                     min = trainingSetXY[j][i];
                 if(trainingSetXY[j][i] > max) //find max value
                     max = trainingSetXY[j][i];
             }
 			float stepSize = (max-min)/numStep; //calculate step size
-            for(int j=-1;j<(int)numStep+1;j++) {
-                for(int k=0;k<2;k++) { //0 = "lt", 1 = "gt"
+            for(int j=-1;j<(int)numStep+1;j++)
+            {
+                for(int k=0;k<2;k++) //0 = "lt", 1 = "gt"
+                {
                     String inequal;
                     if(k==0){inequal = "lt";}
                     else{inequal = "gt";}
@@ -56,15 +62,20 @@ public class DecisionStumps {
                     float threshValue = min+j * stepSize; //calculate threshold v
                     float[] predictedValues = stumpClassify(i,threshValue,inequal); //predict labels
                     int[] errArray = new int[m];
-                    for(int l=0;l<m;l++) { //initialize error array to all 1's
-                        errArray[l] = 1;}
-                    for(int l=0;l<predictedValues.length;l++) {
-                        if(predictedValues[l] == classLabels[l]) { //if predicted labels differs from real, flag a 1 in the error array
+                    for(int l=0;l<m;l++) //initialize error array to all 1's
+                    {
+                        errArray[l] = 1;
+                    }
+                    for(int l=0;l<predictedValues.length;l++)
+                    {
+                        if(predictedValues[l] == classLabels[l]) //if predicted labels differs from real, flag a 1 in the error array
+                        {
                             errArray[l]=0;
                         }
                     }
                     float weightedErr = 0.0f;
-                    for(int l=0;l<errArray.length;l++) {
+                    for(int l=0;l<errArray.length;l++)
+                    {
                         weightedErr += errArray[l] * weights[l];
                     }
                     if(weightedErr<minErr)
@@ -73,7 +84,7 @@ public class DecisionStumps {
                         bestClassEst = (float[])predictedValues.clone();
                     }
                     
-                     /*
+                    /*
                      bestStump['dim'] = i
                      bestStump['thresh'] = threshVal
                      bestStump['ineq'] = inequal*/
@@ -84,25 +95,32 @@ public class DecisionStumps {
 		
 	}
     
-    private float[] stumpClassify(int dimension,float threshVal, String inequal) {
+    private float[] stumpClassify(int dimension,float threshVal, String inequal)
+    {
         float[] retArrary = new float[trainingSetXY.length];
-        if(inequal == "lt") {
-            for(int i=0;i<trainingSetXY.length;i++) {
-                if(trainingSetXY[i][dimension] <= threshVal) {
+        if(inequal == "lt")
+        {
+            for(int i=0;i<trainingSetXY.length;i++)
+            {
+                if(trainingSetXY[i][dimension] <= threshVal)
+                {
                     retArrary[i] = -1.0f;
                 }
             }
         }
-        else {
-            for(int i=0;i<trainingSetXY.length;i++) {
-                if(trainingSetXY[i][dimension] > threshVal) {
+        else
+        {
+            for(int i=0;i<trainingSetXY.length;i++)
+            {
+                if(trainingSetXY[i][dimension] > threshVal)
+                {
                     retArrary[i] = 1.0f;
                 }
             }
         }
         return retArrary;
     }
-
+    
 	private void parseInput(Scanner in)
 	{
 		int i = 0;
