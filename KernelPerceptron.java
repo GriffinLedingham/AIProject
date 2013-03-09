6c65bf10c3bd611ef5cc7980e7a9e01c2dc3f30d
stryder199 <lessardme@hotmail.com>
2013-03-09 13:06:27 -0800

    merge hell

--- KernelPerceptron.java
+++ KernelPerceptron.java
@@ -3,20 +3,27 @@ import java.util.*;
 
 public class KernelPerceptron {
 	private float[][] K;
-    
-    //[height][width]
-    private float[][] data;
-    private float[][] y;
-    private int height,widthx, widthy;
 
-    PrintWriter debugOut;
-    
+	//[height][width]
+	private float[][] data;
+	private float[][] y;
+	private int height,widthx, widthy;
+
+	PrintWriter debugOut;
+
 	public boolean debug = false;
-	
-	public float runPerceptron(String filename, int max_iterations)
+
+	public float runPerceptron(String filename, int max_iterations, int dimension, float minErr)
 	{
 		float testError;
-		
+
+		try {
+			debugOut = new PrintWriter(new FileWriter("debug.txt"));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		// read in the samples
 		File infile = new File(filename);
 		Scanner in;
@@ -27,63 +34,60 @@ public class KernelPerceptron {
 			System.out.println("error reading file");
 			e.printStackTrace();
 		}
-		
+
 		// compute the kernel matrix
-		computeKernel();
-        
+		computeKernel(dimension);
+
 		float[] c = new float[height];
 		//printClassification(c);
-        
-        c= classify(max_iterations);
-		
+
+		c= classify(max_iterations,minErr);
+
 		printClassification(c);
-		
+
 		testError = testError();
-		
+
 		return testError;
 	}
-	
+
 	private float testError()
 	{
 		float error = 0;
 		//TODO
-		
+
 		return error;
 	}
-    
+
 	private void parseInput(Scanner in)
 	{
-        widthx = in.nextInt();
-        widthy = in.nextInt();
-        height = in.nextInt();
-        data = new float[height][widthx];
-        y = new float[height][widthy];
-        
-        for(int i = 0;i<height;i++)
-        {
-            for(int j = 0;j<widthx;j++)
-            {
-                data[i][j] = in.nextFloat();
-                System.out.println(data[i][j]);
-            }
-            for(int j = 0;j<widthy;j++)
-            {
-                y[i][j] = in.nextFloat();
-                System.out.println(y[i][j]);
-            }
-        }
-        
-        normalizeInput();
+		widthx = in.nextInt();
+		widthy = in.nextInt();
+		height = in.nextInt();
+		data = new float[height][widthx];
+		y = new float[height][widthy];
+
+		for(int i = 0;i<height;i++)
+		{
+			for(int j = 0;j<widthx;j++)
+			{
+				data[i][j] = in.nextFloat();
+				System.out.println(data[i][j]);
+			}
+			for(int j = 0;j<widthy;j++)
+			{
+				y[i][j] = in.nextFloat();
+				System.out.println(y[i][j]);
+			}
+		}
+
+		normalizeInput();
 	}
-	
+
 	private void normalizeInput()
 	{
 		float max[] = new float[widthx];
 		float min[] = new float[widthx];
-		float maxy;
-		float miny;
-		//miny = maxy = y[0];
-		
+
 		// for each x attribute
 		for(int i = 0; i < widthx; i++)
 		{
@@ -93,13 +97,16 @@ public class KernelPerceptron {
 			{
 				if (min[i] > data[j][i]) min[i] = data[j][i];
 				if (max[i] < data[j][i]) max[i] = data[j][i];
-				
-				//if (miny > y[j]) miny = y[j];
-				//if (maxy < y[j]) maxy = y[j];
 			}
-			
+
 		}
-		
+
+		System.out.print("Max X Un-normalized Value = " );
+		printArray(max);
+
+		System.out.print("Min X Un-normalized Value = ");
+		printArray(min);
+
 		// normalize the data
 		for(int i = 0; i < widthx; i++)
 		{
@@ -108,34 +115,34 @@ public class KernelPerceptron {
 				data[j][i] = (data[j][i] - (max[i] + min[i])/2)/((max[i] - min[i])/2);
 			}
 		}
-		
-		/* normalize the classifications
+
+	}
+
+	private void computeKernel(int dimension)
+	{
+		K = new float[height][height];
+
+		//For all rows
 		for(int i = 0; i < height; i++)
 		{
-			y[i] = (y[i] - (maxy + miny)/2)/((maxy - miny)/2);
-		} */
-		
+			//Find the dot product between data[i] against every row in the matrix
+			for(int j = 0; j < height; j++)
+			{
+				float dot = 0;
+				try {
+					dot += dot(data[i], data[j]);
+				} catch (IllegalArgumentException e) {
+					System.out.println("Vectors aren't of the same size");
+				}
+
+				K[j][i] = (float)Math.pow(1 + dot, widthx);
+			}
+		}
+
+		debugOut.println("Computed Kernel");
+		print2DArrayFile(K);
 	}
-    
-	private void computeKernel()
-    {
-        K = new float[height][height];
-        
-        for(int i = 0; i < height; i++)
-        {
-            for(int j = 0; j < height; j++)
-            {
-                float dot = 0.0f;
-                for(int k=0;k<widthx;k++)
-                {
-                    dot += data[i][k]*data[j][k];
-                }
-                
-                K[j][i] = (float)Math.pow(1 + dot, widthx);
-            }
-        }
-    }
-	
+
 	private void printClassification(float[] classifcation)
 	{
 		int i;
@@ -146,20 +153,29 @@ public class KernelPerceptron {
 		}
 		System.out.println();
 	}
-    
-	private float[] classify(int max_iterations)
+
+	private float[] classify(int max_iterations, float minErr)
 	{
 		int count = 0;
-        
+
+		int[] errArray = new int[height];
+		for(int i=0; i<height; i++) //initialize error array to all 1's
+		{
+			errArray[i] = 1;
+		}
+
+		float lastErr = 0.0f;
+
 		// the classifier
 		float[] c = new float[height];
 		Arrays.fill(c, 0);
-        
+
 		// loop control
 		boolean misclassified = true;
-        
+
 		while(misclassified && (count < max_iterations))
 		{
+			int errCount=0;
 			count++;
 			misclassified = false;
 			for(int j=0;j<height;j++)
@@ -175,13 +191,97 @@ public class KernelPerceptron {
 					{
 						c[j] += y[j][l];
 						misclassified = true;
-						//errCount++;
+						errCount++;
 					}
 				}
+				
+				System.out.println("Itteration:" + count + " Sum:" + sum + " Missclassified:" + misclassified);
 			}
+
+			float currErr = (float)errCount/((float)height*(float)widthy);
+			System.out.println("Iteration: "+count+", Error:"+currErr);
+			if(currErr <= minErr)
+			{
+				break;
+			}
+			if(count >= max_iterations)
+				System.out.println("broke early");
 		}
-		
+
 		return c;
+	}
+
+	private float sign(float x)
+	{
+		if(x<0)
+			return -1.0f;
+		else if(x>0)
+			return 1.0f;
+		else
+			return 0.0f;
+	}
+
+	private float dot(float[] v1, float[] v2)
+	{
+		int lengthV1 = v1.length;
+		int lengthV2 = v2.length;
+		float result = 0;
+		if(lengthV1 != lengthV2)
+		{
+			throw new IllegalArgumentException();
+		}
+
+		for(int i=0; i<lengthV1; i++)
+		{
+			result += v1[i]*v2[i];
+		}
+
+		return result;
+	}
+
+	private void print2DArray(float[][] data)
+	{
+		for(int i=0; i<data.length; i++)
+		{
+			for(int j=0; j<data[i].length; j++)
+			{
+				System.out.print(data[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+
+	private void print2DArrayFile(float[][] data)
+	{
+		for(int i=0; i<data.length; i++)
+		{
+			for(int j=0; j<data[i].length; j++)
+			{
+				debugOut.print(data[i][j] + " ");
+			}
+			debugOut.println("");
+		}
+	}
+
+	private void printArray(float[] data)
+	{
+		for(int i=0; i<data.length; i++)
+		{
+			System.out.print(data[i] + " ");	
+		}
+		System.out.println("");
+	}
+
+	private float[] transposeRow(float[][] data, int colNum )
+	{
+		float[] result = new float[height];
+
+		for(int i=0; i<height; i++)
+		{
+			result[i] = data[i][colNum];
+		}
+
+		return result;
 	}
 }
 
--- TesterClass.java
+++ TesterClass.java
@@ -14,7 +14,7 @@ public class TesterClass {
 	private void testOne()
 	{
 		float testError;
-		testError = kernelPerceptron.runPerceptron("data.txt", 1000, 2,0.02f);
+		testError = kernelPerceptron.runPerceptron("iris.txt", 1000, 2, 0.02f);
 		//decisionStumps.runDecisionStumps("data.txt", "class.txt", 10);
 		//TODO
 	}
