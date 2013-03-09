7beae153aa34f99688c740e8f30d61413f73f8ab
stryder199 <lessardme@hotmail.com>
2013-03-09 15:40:49 -0800

    whitespace

--- FileFormat.java
+++ FileFormat.java
@@ -13,33 +13,26 @@ public class FileFormat {
 
 	public void format()
 	{
-        try{
-            
-    		File file = new File("iris.txt");
-            
-    		file.delete();
-            
-    	}catch(Exception e){
-            
-    		e.printStackTrace();
-            
-    	}
-        
-        try{
-            
-    		File file = new File("fertility.txt");
-            
-    		file.delete();
-            
-    	}catch(Exception e){
-            
-    		e.printStackTrace();
-            
-    	}
-        
-        
-        
-        
+		try{
+			File file = new File("iris.txt");
+
+			file.delete();
+
+		}catch(Exception e){          
+			e.printStackTrace();  
+		}
+
+		try{  
+			File file = new File("fertility.txt");
+
+			file.delete();
+
+		}catch(Exception e){
+
+			e.printStackTrace();
+
+		}
+
 		String filetext = "";
 		FileInputStream stream = null;
 		try {
@@ -48,8 +41,8 @@ public class FileFormat {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		  try {
-		    MappedByteBuffer bb = null;
+		try {
+			MappedByteBuffer bb = null;
 			try {
 				FileChannel fc = stream.getChannel();
 				bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
@@ -57,70 +50,70 @@ public class FileFormat {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		    /* Instead of using default, pass in a decoder. */
-		    filetext =  Charset.defaultCharset().decode(bb).toString();
-		  }
-		  finally {
-		    try {
+			/* Instead of using default, pass in a decoder. */
+			filetext =  Charset.defaultCharset().decode(bb).toString();
+		}
+		finally {
+			try {
 				stream.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		  }
-		  
-          File f = new File("iris.txt");
-		  
-		  filetext = filetext.replaceAll("Iris-setosa", "-1 -1");
-		  filetext = filetext.replaceAll("Iris-versicolor", "-1 1");
-		  filetext = filetext.replaceAll("Iris-virginica", "1 -1");
-		  filetext = filetext.replaceAll(",", " ");
-		  
-		  BufferedWriter out;
+		}
+
+		File f = new File("iris.txt");
+
+		filetext = filetext.replaceAll("Iris-setosa", "-1 -1");
+		filetext = filetext.replaceAll("Iris-versicolor", "-1 1");
+		filetext = filetext.replaceAll("Iris-virginica", "1 -1");
+		filetext = filetext.replaceAll(",", " ");
+
+		BufferedWriter out;
 		try {
 			out = new BufferedWriter(new FileWriter(f));
-			  out.write("4 2 150\n");
-			  out.write(filetext);
-			  out.close();
+			out.write("4 2 150\n");
+			out.write(filetext);
+			out.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        //==============================================================
-          filetext = "";
-        try {
+		//==============================================================
+		filetext = "";
+		try {
 			stream = new FileInputStream(new File("fertility_Diagnosis.txt"));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        try {
-            FileChannel fc = stream.getChannel();
-            MappedByteBuffer bb = null;
+		try {
+			FileChannel fc = stream.getChannel();
+			MappedByteBuffer bb = null;
 			try {
 				bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-            filetext =  Charset.defaultCharset().decode(bb).toString();
-        }
-        finally {
-		    try {
+			filetext =  Charset.defaultCharset().decode(bb).toString();
+		}
+		finally {
+			try {
 				stream.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }
-        
-        f = new File("fertility.txt");
-        
-        filetext = filetext.replaceAll("N", "-1");
-        filetext = filetext.replaceAll("O", "1");
-        filetext = filetext.replaceAll(",", " ");
-        
-        try {
+		}
+
+		f = new File("fertility.txt");
+
+		filetext = filetext.replaceAll("N", "-1");
+		filetext = filetext.replaceAll("O", "1");
+		filetext = filetext.replaceAll(",", " ");
+
+		try {
 			out = new BufferedWriter(new FileWriter(f));
 			out.write("9 1 100\n");
 			out.write(filetext);
