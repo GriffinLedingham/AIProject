d7970c467e123f08356909c7d3605c8e9c0d0d1b
stryder199 <lessardme@hotmail.com>
2013-03-11 14:59:26 -0700

    Reworked stuff for new assignment! YAY

---
+++ Sudoku/src/TesterClass.java
@@ -0,0 +1,30 @@
+
+public class TesterClass {
+
+	SudokuClass sudoku = new SudokuClass();
+	
+	private void TestOne()
+	{
+		
+	}
+	
+	private void TestTwo()
+	{
+		
+	}
+	
+	public void RunAllTests()
+	{
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TesterClass tester = new TesterClass();
+		
+		tester.RunAllTests();
+	}
+
+}
